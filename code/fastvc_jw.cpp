656bc21c35de05d0449f8e7e78a7bf146ad5ee2b
Sreyans Sipani <sreyans.sipani99@gmail.com>
2020-11-07 17:30:45 +0530

    stuff i tried and some bug fixes'

--- code/fastvc_jw.cpp
+++ code/fastvc_jw.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<ii> vii;
+typedef vector<ll> vl;
+
 #define endl '\n'
 #define umap unordered_map
 #define uset unordered_set
@@ -9,23 +19,13 @@ using namespace std;
 #define LSOne(x) (x & (-x))
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
-#define sz(x) (int) x.size()
-#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define sz(x) (ll) x.size()
+#define rep(i, a, b) for (ll i = a; i < b; ++i)
 #define trav(a, x) for (auto& a : x)
 
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef vector<int> vi;
-typedef vector<double> vd;
-typedef vector<vi> vvi;
-typedef vector<bool> vb;
-typedef vector<vb> vvb;
-typedef vector<ii> vii;
-typedef vector<ll> vl;
-
-int rc_size;
-int usp;
-int N, E;
+ll rc_size;
+ll usp;
+ll N, E;
 
 // for edges
 vii e; //edge list
@@ -49,8 +49,8 @@ vvi AL; // Adjacency List
 ll cost = 0;
 ll best_cost = 0;
 clock_t start = clock();
-uset<int> banlist;
-int seed = 0;
+uset<ll> banlist;
+ll seed = 0;
 
 void fast() {
     cin.tie(0);
@@ -58,16 +58,16 @@ void fast() {
     ios::sync_with_stdio(0);
 }
 
-void add(int v) {
+void add(ll v) {
     if(!cover[v]) {
         cost += W[v];
         cover[v] = true;
     }
     score[v] = -score[v];
     rep(i, 0, deg[v]) {
-        int ei = el[v][i];
+        ll ei = el[v][i];
         ii edge = e[ei];
-        int u = edge.first == v ? edge.second : edge.first;
+        ll u = edge.first == v ? edge.second : edge.first;
         if(cover[u]) {
             score[u] += ew[ei];
         } else {
@@ -78,22 +78,23 @@ void add(int v) {
 
 }
 
-void remove(int v) {
+void remove(ll v) {
     //cout << "Removing " << v << endl;
     if(cover[v]) {
         cost -= W[v];
     } else {
-        cout << "Remove error" << endl;
-        exit(0);
+        // cout << "remove error " << endl;
+        return;
     }
     cover[v] = false;
     score[v] = -score[v];
 
+
     // cout << "removing " << v << " with degree " << deg[v] << endl;
     rep(i, 0, deg[v]) {
-        int ei = el[v][i];
+        ll ei = el[v][i];
         ii edge = e[ei];
-        int u = edge.first == v ? edge.second : edge.first;
+        ll u = edge.first == v ? edge.second : edge.first;
         if(cover[u]) {
             score[u] -= ew[ei];
             // cout << "safe to remove " << v << endl;
@@ -113,31 +114,43 @@ void remove_redundant() {
     }
 }
 
-int choose_min_loss_vertex() { // Choose v in C such that score(v) is the highest negative number
+ll choose_min_loss_vertex() { // Choose v in C such that score(v) is the highest negative number
     double min_loss = -DBL_MAX;
-    int to_remove = -1;
-    rep(i, 0, N) {
+    ll to_remove = -1;
+    int i = 0;
+    while(i < N) {
+        if(cover[i]) {
+            to_remove = i;
+            break;
+        }
+        i++;
+    }
+    while(i < N) {
         if(cover[i]) {
             double ascore = score[i] / W[i];
+            // cout << i << " with score of " << ascore << endl;
             if(ascore > min_loss) {
                 to_remove = i;
                 min_loss = ascore;
             }
         }
+        i++;
     }
     return to_remove;
 }
 
-int choose_bms_vertex() { // 50 Random candidates not in tabu, picking the candidate with the highest negative number
+ll choose_bms_vertex() { // 50 Random candidates not in tabu, picking the candidate with the highest negative number
     vi candidates;
     rep(i, 0, N) {
         if(cover[i] && !tabu[i]) candidates.pb(i);
     }
-
     shuffle(candidates.begin(), candidates.end(), default_random_engine(seed));
     double worst_score = -DBL_MAX;
-    int to_remove = -1;
-    rep(i, 0, min(sz(candidates), 50)) {
+    if(sz(candidates) == 0) {
+        return -1;
+    }
+    ll to_remove = candidates[0];
+    rep(i, 0, min(sz(candidates), (ll)50)) {
         double ascore = score[candidates[i]] / W[candidates[i]];
         if(ascore > worst_score) {
             worst_score = ascore;
@@ -148,51 +161,54 @@ int choose_bms_vertex() { // 50 Random candidates not in tabu, picking the candi
 
 }
 
-vi get_uncovered(int w, int u) {
+vi get_uncovered(ll w, ll u) {
     vi edge_indices;
     trav(i, el[w]) {
-        int a = e[i].first;
-        int b = e[i].second;
+        ll a = e[i].first;
+        ll b = e[i].second;
         if(!cover[a] && !cover[b]) edge_indices.pb(i);
     }
     trav(i, el[u]) {
-        int a = e[i].first;
-        int b = e[i].second;
+        ll a = e[i].first;
+        ll b = e[i].second;
         if(!cover[a] && !cover[b]) edge_indices.pb(i);
     }
     return edge_indices;
 }
 
-int choose_max_gain_vertex() {
+ll choose_max_gain_vertex() {
     double max_gain = 0;
-    int to_add = -1;
+    ll to_add = -1;
     rep(i, 0, N) {
         if(!cover[i]) {
             double ascore = score[i] / W[i]; 
-            if(ascore > max_gain) {
+            if(ascore >= max_gain) {
                 to_add = i;
                 max_gain = ascore;
             }
         }
     }
-
-    // In case no candidates
-    if(to_add == -1) {
-        cout << "Choose max gain error" << endl;
-        exit(0);
-    }
-
+    // In case no candidates    
+    // if(to_add == -1) {  
+    //     rep(i, 0, N) {  
+    //         if(!cover[i]) { 
+    //             to_add = i; 
+    //             break;  
+    //         }   
+    //     }   
+    // }   
+    
     return to_add;
 }
 
-void update_cc_remove(int v) {
+void update_cc_remove(ll v) {
     cc[v] = 0;
     trav(i, AL[v]) {
         cc[i] = 1;
     }
 }
 
-void update_cc_add(int v) {
+void update_cc_add(ll v) {
     trav(i, AL[v]) {
         cc[i] = 1;
     }
@@ -235,11 +251,11 @@ void get_optimal_vertices() {
             } // Rule 3 End
         }
     }
-    int x = sz(twoNeighbours);
+    ll x = sz(twoNeighbours);
     rep(i, 0, x-1) { // Rule 4
         rep(j, i+1, x) {
-            int u = twoNeighbours[i];
-            int v = twoNeighbours[j];
+            ll u = twoNeighbours[i];
+            ll v = twoNeighbours[j];
             if(AL[u] == AL[v] && // / N(u) = N(v) ={n1, n2}
             ((W[u] + W[v]) >= (W[AL[u][0]] + W[AL[u][1]])) && // W(u) + W(v) >= W(n1) + W(n2)
             (!AM[AL[u][0]][AL[u][1]])) { // {n1, n2} !∈ E
@@ -250,11 +266,11 @@ void get_optimal_vertices() {
     }
 }
 
-int check_best_cover() {
+ll check_best_cover() {
     rep(i, 0, E) {
         ii edge = e[i];
-        int v = edge.first;
-        int u = edge.second;
+        ll v = edge.first;
+        ll u = edge.second;
         if(!(best_cover[v] || best_cover[u])) {
             return 0;
         }
@@ -262,11 +278,11 @@ int check_best_cover() {
     return 1;
 }
 
-int check_cover() {
+ll check_cover() {
     rep(i, 0, E) {
         ii edge = e[i];
-        int v = edge.first;
-        int u = edge.second;
+        ll v = edge.first;
+        ll u = edge.second;
         if(!(cover[v] || cover[u])) {
             return 0;
         }
@@ -275,6 +291,7 @@ int check_cover() {
 }
 
 void update_best_cover() {
+    // cout << "check_cover = " << check_cover() << " and " << cost << " < " << best_cost << " = " << (cost < best_cost) << endl;
     if(check_cover() == 1 && cost < best_cost) {
         best_cost = cost;
         best_cover = cover;
@@ -282,9 +299,14 @@ void update_best_cover() {
 }
 
 int main() {
-    fast();
+    // fast();
     srand(time(NULL));
     cin >> N >> E;
+    if(N == 0 || E == 0) {
+        cout << 0 << endl;
+        cout << endl;   
+        return 0;
+    }
     W.assign(N, -1);
     score.assign(N, 0);
     deg.assign(N, 0);
@@ -295,9 +317,9 @@ int main() {
     trav(i, W) cin >> i;
     AM.assign(N, vb(N, false)); // We store both Adjacency Matrix
     AL.assign(N, vi()); // and Adjacency List
-    int ei = 0;
+    ll ei = 0;
     rep(i, 0, E) {
-        int v, u;
+        ll v, u;
         cin >> v >> u;
         if(!AM[v][u] && !AM[u][v]) { 
             e.eb(v, u);
@@ -317,8 +339,8 @@ int main() {
     best_cover.assign(N, false);
     rep(i, 0, E) {
         ii edge = e[i];
-        int v = edge.first;
-        int u = edge.second;
+        ll v = edge.first;
+        ll u = edge.second;
         if(!(cover[u] || cover[v])) {
             double vs = (double)deg[v] / W[v];
             double us = (double)deg[u] / W[u];
@@ -334,7 +356,7 @@ int main() {
     best_cover = cover;
     best_cost = cost;
 
-    int times = 50;
+    ll times = 50;
     vi b(E/1024 + 1);
     rep(i, 0, E/1024 + 1) {
         b[i] = i;
@@ -345,9 +367,9 @@ int main() {
         cost = 0;
         shuffle(b.begin(), b.end(), default_random_engine(seed));
         trav(block, b) {
-            int beg = block * 1024;
-            int end = min(E, beg + 1024);
-            int bs = end - beg + 1;
+            ll beg = block * 1024;
+            ll end = min(E, beg + 1024);    
+            ll bs = end - beg + 1;
             vi index(bs);
             rep(j, 0, bs) {
                 index[j] = j + beg;
@@ -357,10 +379,10 @@ int main() {
                 cost += W[i];
             }
             while(bs > 0) {
-                int ri = rand() % bs;
+                ll ri = rand() % bs;
                 ii edge = e[index[ri]];
-                int v = edge.first;
-                int u = edge.second;
+                ll v = edge.first;
+                ll u = edge.second;
                 swap(index[ri], index[--bs]);
                 if(!(cover[u] || cover[v])) {
                     double vs = (double)deg[v] / W[v];
@@ -375,38 +397,34 @@ int main() {
                 }
             }
         }
-        update_best_cover();
+        update_best_cover(); // probably want to remove redundant here.
     }
 
     
     // Naive
     cover.assign(N, false);
-    ll cost = 0;
+    cost = 0;
     trav(i, e) {
-        int u = i.first; int v = i.second;
-        if(W[u] < W[v]) {
-            cover[u] = true;
-            cost += W[u];
-        } else if(W[u] > W[v]) {
-            cover[v] = true;
-            cost += W[v];
-        } else if(u < v) {
-            cover[u] = true;
-            cost += W[u];
-        } else {
-            cover[v] = true;
-            cost += W[v];
+        ll u = i.first; ll v = i.second;
+        if(!cover[u] && !cover[v]) {
+            if(W[u] < W[v]) {
+                cover[u] = true;
+                cost += W[u];
+            } else if(W[u] > W[v]) {
+                cover[v] = true;
+                cost += W[v];
+            } else if(u < v && deg[u] > 1) {
+                cover[u] = true;
+                cost += W[u];
+            } else {
+                cover[v] = true;
+                cost += W[v];
+            }
         }
     }
     update_best_cover();
-    // Naive end
-    
-
-
     cover = best_cover;
     cost = best_cost;
-
-    
 
     // if(check_best_cover() == 0) {
     //     cout << "wrong soln here 1" << endl;
@@ -415,8 +433,8 @@ int main() {
 
     rep(i, 0, E) {
         ii edge = e[i];
-        int v = edge.first;
-        int u = edge.second;
+        ll v = edge.first;
+        ll u = edge.second;
         // cout << u << " - " << v << endl;
         if(cover[u] && !cover[v]) {
             // cout << " covered by " << u << endl;
@@ -427,10 +445,8 @@ int main() {
         }
     }
 
-
     remove_redundant();
-
-    
+    update_best_cover();    
 
     // if(check_best_cover() == 0) {
     //     cout << "wrong soln here 2" << endl;
@@ -439,28 +455,49 @@ int main() {
 
     cc.assign(N, 1);
 
-    int ctr = 0;
-    
-    while(((float(clock() - start) /  CLOCKS_PER_SEC) < 1.95)) {
+    ll ctr = 0;
+
+    while(((float(clock() - start) /  CLOCKS_PER_SEC) < 1.90)) {
         // Choose vertices to remove
-        int w = choose_min_loss_vertex();
-        if(w == -1) break;
+        ll w = choose_min_loss_vertex();
+        if(w == -1) {
+            // cout << "choose_min_loss error" << endl; 
+            break;
+        }
         remove(w);
         update_cc_remove(w);
 
-        int u = choose_bms_vertex();
-        if(u == -1) break;
+
+        ll u = choose_bms_vertex();
+        if(u == -1) {
+            // cout << "choose_bms error" << endl; 
+            break;
+        }
         remove(u);
+
         update_cc_remove(u);
+
         tabu.assign(N, 0);
         // Generate Uncovered edges 
 
         vi uncovered_edges = get_uncovered(u, w);
-        // cout << "We removed vertices " << w << " and " << u << endl; 
+        // cout << "We removed vertices " << w << " and " << u << " and have " << sz(uncovered_edges) << endl; 
         
         // Choose vertices to add
         while(sz(uncovered_edges) > 0) {
-            int v = choose_max_gain_vertex();
+            ll v = choose_max_gain_vertex();
+            if(v == -1) {
+                // cout << "max_gain error" << endl;
+                cout << best_cost << endl;
+                rep(i, 0, N) {
+                    if(best_cover[i]) {
+                        cout << i << " ";
+                    }
+                }
+                cout << endl;
+                // cout << float(clock() - start ) /  CLOCKS_PER_SEC << " " << ctr;
+                return 0;
+            }
             add(v);
             vi new_ue;
             // Update edges after adding
@@ -481,23 +518,27 @@ int main() {
                 cc[edge.first] = 1;
                 cc[edge.second] = 1;
             }
+            // cout << "here 3 " << sz(uncovered_edges) << endl;
             // cout << "We added vertice  " << v << endl;
         }
         
         // Clean up and compare
         
         remove_redundant();
+
+
         update_best_cover();
+
 
         //cout << "At iteration " << ctr << " cost is " << cost << endl; 
         ctr++;
     }
     
 
-    if(check_best_cover() == 0) {
-         cout << "wrong soln here 3" << endl;
-         return 0;
-     }
+    // if(check_best_cover() == 0) {
+    //      cout << "wrong soln here 3" << endl;
+    //      return 0;
+    //  }
 
     cout << best_cost << endl;
     rep(i, 0, N) {
@@ -506,7 +547,6 @@ int main() {
         }
     }
     cout << endl;
-    //cout << float(clock() - start ) /  CLOCKS_PER_SEC;
-        
-
+    // cout << float(clock() - start ) /  CLOCKS_PER_SEC << " " << ctr;
+    return 0;
 }
